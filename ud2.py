585bb1df58447f682cbdd3d2b5eb27a6ff144d3b
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2024-01-19 19:27:20 -0600

    Update ud2.py

--- ud2.py
+++ ud2.py
@@ -1,4 +1,4 @@
-ata.columns= ["States","Date","Frequency",
+data.columns= ["States","Date","Frequency",
                "Estimated Unemployment Rate",
                "Estimated Employed",
                "Estimated Labour Participation Rate",
