17ac7e59f960afb3407bbe4513f2ac4cd91e9b6e
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2024-01-19 19:28:19 -0600

    Create ud6.py

---
+++ ud6.py
@@ -0,0 +1,6 @@
+unemploment = data[["States", "Region", "Estimated Unemployment Rate"]]
+figure = px.sunburst(unemploment, path=["Region", "States"], 
+                     values="Estimated Unemployment Rate", 
+                     width=700, height=700, color_continuous_scale="RdY1Gn", 
+                     title="Unemployment Rate in India")
+figure.show()
