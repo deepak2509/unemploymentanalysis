cd587e63dd65d25b336c458d5c7b4fed6e3a1038
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2024-01-19 19:27:06 -0600

    Create ud4.py

---
+++ ud4.py
@@ -0,0 +1,7 @@
+data.columns= ["States","Date","Frequency",
+               "Estimated Unemployment Rate","Estimated Employed",
+               "Estimated Labour Participation Rate","Region",
+               "longitude","latitude"]
+plt.title("Indian Unemployment")
+sns.histplot(x="Estimated Employed", hue="Region", data=data)
+plt.show()
