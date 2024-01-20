5ea1555ba40bad936255aee61b1cddfebcb43411
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2024-01-19 19:26:01 -0600

    Create ud2.py

---
+++ ud2.py
@@ -0,0 +1,5 @@
+ata.columns= ["States","Date","Frequency",
+               "Estimated Unemployment Rate",
+               "Estimated Employed",
+               "Estimated Labour Participation Rate",
+               "Region","longitude","latitude"]
