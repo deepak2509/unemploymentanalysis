5af0c8f407df06405d697459cdabb764e6168d3b
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2024-01-19 19:24:47 -0600

    Create ueanalysis.py

---
+++ ueanalysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+data = pd.read_csv("Users/deepakgadde/Desktop/unemployment.csv")
+print(data.head())
